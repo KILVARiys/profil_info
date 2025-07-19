597259d5c153922bfdb4792f1e9771d8819e3819
KILVARiys <seremenko135@gmail.com>
2025-07-19 13:11:07 +0300

    changed

--- gui/gui.py
+++ gui/gui.py
@@ -1,13 +1,7 @@
 import os
 from PySide6.QtWidgets import (
-    QWidget,
-    QListWidget,
-    QListWidgetItem,
-    QLabel,
-    QPushButton,
-    QVBoxLayout,
-    QHBoxLayout,
-    QLineEdit
+    QWidget, QListWidget, QListWidgetItem, QLabel, QPushButton,
+    QVBoxLayout, QHBoxLayout, QLineEdit
 )
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QColor, QIcon
@@ -21,9 +15,9 @@ class Widget(QWidget):
         base_path = os.path.dirname(os.path.abspath(__file__))
         icon_path = os.path.join(base_path, "icons", "")
 
-        # Создание QListWidget с элементами
-        menu_widget = QListWidget()
-        menu_widget.setIconSize(QSize(48, 48))
+        # --- Боковая панель с меню ---
+        self.menu_widget = QListWidget()
+        self.menu_widget.setIconSize(QSize(48, 48))
         
         cs = QListWidgetItem(" CS 2")
         cs.setIcon(QIcon(icon_path + "cs2.jpg"))
@@ -46,14 +40,23 @@ class Widget(QWidget):
         steam.setTextAlignment(Qt.AlignCenter)
 
         # Добавляем элементы по одному
-        menu_widget.addItem(cs)
-        menu_widget.addItem(dota)
-        menu_widget.addItem(rust)
-        menu_widget.addItem(steam)
+        self.menu_widget.addItem(cs)
+        self.menu_widget.addItem(dota)
+        self.menu_widget.addItem(rust)
+        self.menu_widget.addItem(steam)
 
-        for i in range(menu_widget.count()):
-            item = menu_widget.item(i)
+        for i in range(self.menu_widget.count()):
+            item = self.menu_widget.item(i)
             item.setTextAlignment(Qt.AlignCenter)
+
+        # --- Кнопка в верхней части окна ---
+        self.toggle_button = QPushButton()
+        self.toggle_button.setIcon(QIcon(icon_path + "menu.png"))  # Указываем иконку menu.png
+        self.toggle_button.setIconSize(QSize(32, 32))
+        self.toggle_button.setFixedSize(48, 48)
+        self.toggle_button.setCheckable(True)
+        self.toggle_button.setChecked(True)
+        self.toggle_button.toggled.connect(self.toggle_menu)
 
         # --- Верхнее поле поиска ---
         self.search_input = QLineEdit()
@@ -79,9 +82,35 @@ class Widget(QWidget):
         main_widget = QWidget()
         main_widget.setLayout(content_layout)
 
+        # --- Макет боковой панели (чтобы можно было скрыть/показать) ---
+        side_layout = QVBoxLayout()
+        side_layout.addWidget(self.menu_widget)
+        side_layout.setContentsMargins(0, 0, 0, 0)
+        side_layout.setSpacing(0)
+
+        side_widget = QWidget()
+        side_widget.setLayout(side_layout)
+
         # --- Основной макет окна ---
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
+
+        # --- Верхняя панель с кнопкой меню ---
+        top_toolbar = QHBoxLayout()
+        top_toolbar.addWidget(self.toggle_button)
+        top_toolbar.addStretch()
+
+        main_layout.addLayout(top_toolbar)
+
+        # --- Горизонтальный макет: боковая панель + основное содержимое ---
         layout = QHBoxLayout()
-        layout.addWidget(menu_widget, 1)
+        layout.addWidget(side_widget, 1)
         layout.addWidget(main_widget, 4)
 
-        self.setLayout(layout)
+        main_layout.addLayout(layout)
+
+        self.setLayout(main_layout)
+
+    def toggle_menu(self, checked):
+        self.menu_widget.setVisible(checked)
